fe834ec6ec62e0ab5598623f5ec55e99b723eb11
SeverinuM <62368769+severinum@users.noreply.github.com>
2023-07-02 00:16:03 +0100

    Issue 7 add GitHub workflows to build docker images (#9)
    * Add GitHub workflow to build docker image and push to Quay.io
    
    * Add Header tests
    
    * Add Jest to Lint
    
    * Add Dockerfile
    
    * Add default.conf for Nginx image
    
    * User multiarch image build step
    
    * Add IMAGE_TAG env var to workflow
    
    * Change tag ticket to issue
    
    * Add login to Quay step
    
    * Add quay registry to login to quay step
    
    * Change repo and registry names to quay
    
    * Change Dockerfile path
    
    * Clean Docker.yml workflow
    
    ---------
    
    Co-authored-by: sewerynmi <sewerynmichota@gmai.com>

---
+++ src/test/helpers/Helpers.jsx
@@ -0,0 +1,28 @@
+import { render, waitFor } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { ReactKeycloakProvider } from '@react-keycloak/web';
+
+export const renderWithProviders = (Component, AuthClientStub) =>
+    render(
+        <ReactKeycloakProvider authClient={AuthClientStub}>
+            <BrowserRouter>{Component}</BrowserRouter>
+        </ReactKeycloakProvider>
+    );
+
+export const renderWithRouter = (
+    Component,
+    {
+        route = '/',
+        history = createMemoryHistory({ initialEntries: [route] }),
+    } = {}
+) => {
+    return {
+        ...render(<BrowserRouter history={history}>{Component}</BrowserRouter>),
+        history,
+    };
+};
+
+export const expectNeverToHappen = async (callable, timeout = 100) => {
+    await expect(waitFor(callable, { timeout })).rejects.toBeTruthy();
+};
